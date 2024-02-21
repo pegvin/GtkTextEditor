1d2b2609c42db9e1398a3f3678209e30e6daa542
Aditya <75035219+pegvin@users.noreply.github.com>
2024-02-22 02:06:45 +0530

    Fix Build Errors

--- src/main.c
+++ src/main.c
@@ -1,5 +1,4 @@
 #include <gtk/gtk.h>
-#include <gio/gioenums.h>
 
 static void print_hello(GtkWidget* widget, gpointer data) {
 	(void)widget; (void)data;
@@ -26,7 +25,14 @@ int main(int argc, char** argv) {
 	GtkApplication *app;
 	int status;
 
-	app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+	GApplicationFlags flags;
+#if GLIB_CHECK_VERSION(2, 74, 0)
+	flags = G_APPLICATION_DEFAULT_FLAGS;
+#else
+	flags = G_APPLICATION_FLAGS_NONE;
+#endif
+
+	app = gtk_application_new("org.gtk.example", flags);
 	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 	status = g_application_run(G_APPLICATION(app), argc, argv);
 	g_object_unref(app);
